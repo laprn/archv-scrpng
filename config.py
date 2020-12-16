cee725de3b1245ac969b3561164206c3b48c1eee
laprn <reg@dymcom.net>
2020-12-16 10:07:10 +0900

    firstcommit

---
+++ config.py
@@ -0,0 +1,10 @@
+#Micro CMS parameter
+URL = 'https://laprn.microcms.io/api/v1/blog'
+X_WRITE_API_KEY = '121edb8a-0461-4e89-8bfb-6fe8a160ad81'
+DRIVER_PATH = 'C:/Apps/chromedriver_win32/chromedriver.exe'
+
+#DropBox Access Token
+TOKEN = 'oEwsksKnz2sAAAAAAAAAAfa52oL8YSO4j4GrQV-HYA5flOlx_4ezAzqx5vxMJTq2'
+
+#Netlify build hook
+hook_url = 'https://api.netlify.com/build_hooks/5fad3a7dad135e22ace834e2'
